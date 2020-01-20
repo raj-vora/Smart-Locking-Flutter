fa5f88f5796fc377dc72b5d4ceb996d030493ed0
raj-vora <raj.jv@somaiya.edu>
2020-01-20 13:54:36 +0530

    get imei and uniqueid

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,25 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:smart_lock/auth.dart';
+import 'package:imei_plugin/imei_plugin.dart';
+import 'package:smart_lock/constants.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({this.auth, this.onSignedOut});
   final BaseAuth auth;
   final VoidCallback onSignedOut;
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   void _signOut() async {
     try {
-      await auth.signOut();
-      onSignedOut();
+      await widget.auth.signOut();
+      widget.onSignedOut();
     } catch (e) {
       print(e);
     }
   }
-  
+
+  String _platformImei = 'Unknown';
+  String uniqueId = "Unknown";
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformImei;
+    String idunique;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformImei = await ImeiPlugin.getImei( shouldShowRequestPermissionRationale: false );
+      idunique = await ImeiPlugin.getId();
+    } catch(e) {
+      platformImei = e;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformImei = platformImei;
+      uniqueId = idunique;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Smart Lock'),
+        title: Text('Smart Lock - Test'),
         actions: <Widget>[
           FlatButton(
             child: Text('Logout', style: TextStyle(fontSize: 17.0, color: Colors.white,)),
@@ -27,9 +67,27 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: new Container(
-        child: Center(
-          child: Text('Welcome', style: TextStyle(fontSize: 32.0),),
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: kBackground,
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text('IMEI: $_platformImei\nDevice UniqueID: $uniqueId', style: TextStyle(fontSize: 20.0),),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
