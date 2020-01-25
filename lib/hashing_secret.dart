3036057843e7dc5d1fea0c05e9dd52d762c70425
raj-vora <raj.jv@somaiya.edu>
2020-01-25 12:30:14 +0530

    sending chirp registration done

---
+++ lib/hashing_secret.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
+String hashSecret (String _userSecret) {
+    var temp = new DateTime.now().millisecondsSinceEpoch;
+    double time = temp/100000;
+    String timestamp = time.toString();
+    
+    var key = utf8.encode(timestamp);
+    var secret = utf8.encode(_userSecret);
+
+    var hmacSha1 = new Hmac(sha1, key); // HMAC-SHA1
+    var digest = hmacSha1.convert(secret);
+    
+    print("HMAC digest as bytes: ${digest.bytes}");
+    print("HMAC digest as hex string: $digest");
+    return digest.toString().substring(1,16);
+  }
--- lib/registration.dart
+++ lib/registration.dart
@@ -5,6 +5,13 @@ import 'package:imei_plugin/imei_plugin.dart';
 import 'package:smart_lock/constants.dart';
 import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:chirp_flutter/chirp_flutter.dart';
+import 'package:smart_lock/hashing_secret.dart';
+import 'dart:convert';
 
 class Registration extends StatefulWidget {
   Registration({this.auth});
@@ -17,12 +24,22 @@ class _RegistrationState extends State<Registration> {
   final formKey = GlobalKey<FormState>();
   final db = Firestore.instance;
   String _name, _deviceId, _mobileNumber, _authId, _userId, _emailId, _userSecret, _homeId;
-  //String _platformImei = 'Unknown';
+  String _appKey = 'a7cbAC032bad0FBbCA0bAE528';
+  String _appSecret = 'ac0E3e41c3AfFBE3CED431e5CE4Eee8aC1e793BF353a42bd7E';
+  String _appConfig = 'aTEFRnJLaqzx7nF0U9nb4+SLAjabUC3wBgvMu+0K9LOgGneO16xBh/9WxlkwVf3IRX9mtM1e1aLStrd4jHCFpTm6RLMsAU/bTf4OzxhB8trz0UXvjO2kRIXPuUiaLrc5I1Ekm1wpgtVW3S+dy3SPoe1/eWo9kj6JWUNfNZZdcgKwxhyeI/j9NBNTxp/NFdtFSjQRpuDjxZkw1Ttf/cBDDY0X3FlaG+7j3/OaPa/plVtAMe7Enxjt2CQ6Eg10pzei1tP7RoK/A88EH8RDHmEBCklZGMLmU8RsE08Wv3wEywbc5jG06Edc+KudW19xo7Ab/h2ZHcohkVMjbuO5QkmiH2fGaXNR/0rsKc26q/L740Zsfrw2BoI3mhYEWvYQaHz4LQoD+OrtYvtcasuAlpkjYrlhUo/wUrB4TdLOkPLX4JImmaJZGqmGrHS0NBP9GEhj4c3M3qTEX4MZuU/ai/tWGZEs/grtqbwbOKi0fWwroBUJp1Ba2Edh50KnhcoT2jw3OF6yCZSWPotD9ui/OIbNkdvU2M+ZU7X4+wXtP2IGGz57xCRpNjjeYoxygOao/7DIx8fRaznDgETcgFTRmyfgaMtcGcgwQn3xff9N5nLIFhqfiaZ+UMl8LqfNAsIgqJz5rLPFSHNMGNf1PgTOUF48pLAK7pM10fSMKA38ZEihX9soBaRKwT4L0cAN7e2eG74HPC6jHxxqUrQOBZMjS8x7MUCpnQd7SuSglILXpPZfclVEXVVHlYGnKkAt7xK1iJr0/a+TbP2Jh0csVVkS6s7oK1oqo2gv2J6itQ1dHCtphP1jUja4WtXXBCMaV48h0fNUW8f9oQVhnuB7+CeoOwPE6IuWdrtNxDs+hMwainS/dxs=';
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    _requestPermissions();
+  }
+
+  Future<void> _requestPermissions() async {
+    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.microphone);
+    if (permission.toString() != 'granted') {
+      await PermissionHandler().requestPermissions([PermissionGroup.microphone]);
+    }
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -36,6 +53,10 @@ class _RegistrationState extends State<Registration> {
       idunique = await ImeiPlugin.getId();
       user  = await widget.auth.currentUser();
       email = await widget.auth.getEmailId();
+      // Init ChirpSDK
+      await ChirpSDK.init(_appKey, _appSecret);
+      // Set SDK config
+      await ChirpSDK.setConfig(_appConfig);
     } catch(e) {
       //platformImei = e;
     }
@@ -65,6 +86,8 @@ class _RegistrationState extends State<Registration> {
     _userSecret = secret;
   }
 
+  
+
   bool validateAndSave() {
     final form = formKey.currentState;
     if(form.validate()) {
@@ -76,6 +99,9 @@ class _RegistrationState extends State<Registration> {
 
   void sendChirp() async {
     createUserId();
+    String hashedSecret = hashSecret(_userSecret);
+    String payload ='r' + _userId + hashedSecret;
+    Uint8List _chirpData = utf8.encode(payload);
     var json = {
       'userId' : _userId,
       'authId' : _authId,
@@ -87,14 +113,41 @@ class _RegistrationState extends State<Registration> {
       'homeId' : _homeId
     };
     if(validateAndSave()){
-      await db.collection('users').add(json).then((documentReference) {
-        print(json);
-        print(documentReference.documentID);
-      }).catchError((e) {print(e);});
+      try {
+        await db.collection('users').add(json).then((documentReference) {
+          print(documentReference.documentID);
+        }).catchError((e) {print(e);});
+        await ChirpSDK.start();
+        await ChirpSDK.send(_chirpData);
+        print(_chirpData);
+        Fluttertoast.showToast(
+          msg: "Chirp Sent",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIos: 3,
+          backgroundColor: Colors.white,
+          textColor: Colors.blue,
+          fontSize: 16.0
+        );
+      }
+      catch (e) {
+        print(e);
+      }
     }
   }
 
-  void goToHome() {}
+  void goToHome() async {
+    await ChirpSDK.stop();
+    Fluttertoast.showToast(
+          msg: "SDK Stopped",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIos: 3,
+          backgroundColor: Colors.white,
+          textColor: Colors.black,
+          fontSize: 16.0
+        );
+  }
   
   @override
   Widget build(BuildContext context) {
