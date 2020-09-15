66ad9b3997baa2a4d0ccefb4030d8a1a3c88676d
raj-vora <raj.jv@somaiya.edu>
2020-09-15 10:34:37 +0530

    latest changes 22/6/20

---
+++ lib/components/logo.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Logo = Text(
+  'Smart Lock',
+  style: TextStyle(
+    color: Colors.white,
+    fontFamily: 'OpenSans',
+    fontSize: 50.0,
+    fontWeight: FontWeight.bold,
+  ),
+);
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:smart_lock/components/logo.dart';
 import 'package:smart_lock/constants/auth.dart';
 import 'package:smart_lock/constants/ui_constants.dart';
 
@@ -92,183 +93,138 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: kBackground,
-                padding: EdgeInsets.only(left: 10.0,right: 10.0,top: 10.0),
-                child: Form(
-                  key: formKey,
-                  child: Center(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: logo() + buildInputs() + buildSubmitButtons()
+      body: Container(
+        decoration: kBackground,
+        padding: EdgeInsets.only(left: 10.0,right: 10.0,top: 10.0),
+        child: Form(
+          key: formKey,
+          child: Center(
+            child: ListView(
+              children: <Widget>[
+                Logo,
+                SizedBox(height: 40.0,),
+                Text(
+                  'Email',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: kBoxDecorationStyle,
+                  height: 60.0,
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (input) => !input.contains('@') ? 'Please enter a valid email' : null,
+                    onSaved: (input) => _email = input,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 14.0),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.white,
                       ),
+                      hintText: 'Enter your Email',
+                      hintStyle: kHintTextStyle,
                     ),
                   ),
                 ),
+              SizedBox(height: 30.0,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Password',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white70,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10.0,),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    decoration: kBoxDecorationStyle,
+                    height: 60.0,
+                    child: TextFormField(
+                      obscureText: passwordVisible,
+                      validator: (input) => input.length<8 ? 'Password cannot be less than 8 characters' : null,
+                      onSaved: (input) => _password = input,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'OpenSans',
+                      ),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(top: 14.0),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                        ),
+                        hintText: 'Enter your Password',
+                        hintStyle: kHintTextStyle,
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                            color: Colors.white
+                          ), 
+                          onPressed: () {
+                            setState(() {passwordVisible = !passwordVisible;});
+                          }
+                        )
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+              _formType == FormType.login 
+              ? Column(
+                children: <Widget>[Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text('Forgot Password?', style: TextStyle(fontSize: 15.0, color: Colors.white)),
+                      onPressed: forgotPassword,  
+                    ),
+                  ],
+                ),
+                RaisedButton(
+                  child: Text('Login', style: TextStyle(fontSize: 20.0)),
+                  onPressed: validateAndSubmit,
+                ),
+                FlatButton(
+                  child: Text('New User? Create an Account', style: TextStyle(fontSize: 15.0, color: Colors.white)),
+                  onPressed: moveToRegister,
+                )
+                ]
+                )
+                : Column(children: <Widget>[
+                  SizedBox(height: 20.0,),
+                  RaisedButton(
+                    child: Text('Register', style: TextStyle(fontSize: 20.0)),
+                    onPressed: validateAndSubmit,
+                  ),
+                  FlatButton(
+                    child: Text('Already a user? Login', style: TextStyle(fontSize: 15.0, color: Colors.white)),
+                    onPressed: moveToLogin,
+                  )
+                ]
+                ),  
+              ]
+            ),
           ),
         ),
       )
     );
-  }
-  
-  List<Widget> logo() {
-    return [
-      Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-      Text(
-        'Smart Lock',
-        style: TextStyle(
-          color: Colors.white,
-          fontFamily: 'OpenSans',
-          fontSize: 50.0,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      ],
-    ),
-    ];
-  }
-
-  List<Widget> buildInputs() {
-    return[
-      SizedBox(height: 40.0,),
-      Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'Email',
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.white70,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        SizedBox(height: 10.0,),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 60.0,
-          child: TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            validator: (input) => !input.contains('@') ? 'Please enter a valid email' : null,
-            onSaved: (input) => _email = input,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.email,
-                color: Colors.white,
-              ),
-              hintText: 'Enter your Email',
-              hintStyle: kHintTextStyle,
-            ),
-          ),
-        ),
-      ],
-    ),
-    SizedBox(height: 30.0,),
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'Password',
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.white70,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        SizedBox(height: 10.0,),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 60.0,
-          child: TextFormField(
-            obscureText: passwordVisible,
-            validator: (input) => input.length<8 ? 'Password cannot be less than 8 characters' : null,
-            onSaved: (input) => _password = input,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.lock,
-                color: Colors.white,
-              ),
-              hintText: 'Enter your Password',
-              hintStyle: kHintTextStyle,
-              suffixIcon: IconButton(
-                icon: Icon(
-                  passwordVisible
-                  ? Icons.visibility
-                  : Icons.visibility_off,
-                  color: Colors.white
-                ), 
-                onPressed: () {
-                  setState(() {passwordVisible = !passwordVisible;});
-                }
-              )
-            ),
-          ),
-        ),
-      ],
-    ),
-    ];
-  }
-
-  List<Widget> buildSubmitButtons() {
-    if(_formType == FormType.login){
-      return [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            FlatButton(
-              child: Text('Forgot Password?', style: TextStyle(fontSize: 15.0, color: Colors.white)),
-              onPressed: forgotPassword,  
-            ),
-          ],
-        ),
-        RaisedButton(
-          child: Text('Login', style: TextStyle(fontSize: 20.0)),
-          onPressed: validateAndSubmit,
-        ),
-        FlatButton(
-          child: Text('New User? Create an Account', style: TextStyle(fontSize: 15.0, color: Colors.white)),
-          onPressed: moveToRegister,
-        )
-      ];
-    } else{
-      return [
-        SizedBox(height: 20.0,),
-        RaisedButton(
-          child: Text('Register', style: TextStyle(fontSize: 20.0)),
-          onPressed: validateAndSubmit,
-        ),
-        
-        FlatButton(
-          child: Text('Already a user? Login', style: TextStyle(fontSize: 15.0, color: Colors.white)),
-          onPressed: moveToLogin,
-        )
-      ];
-    }
   }
 }
